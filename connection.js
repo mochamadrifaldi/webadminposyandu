69444952d0c574cc8b0eeaae1791c304dbf378ef
rifaldi18 <mochamadrifaldi75@gmail.com>
2021-07-27 16:34:57 +0700

    second commit

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 /* file: connection.js */
 const MongoClient = require("mongodb").MongoClient
 const connectionString =
-"mongodb://user_latihan:123456@localhost:27017?authSource=admin";
+"mongodb+srv://aldi:Railfansdaop1@wpu.sjkqm.mongodb.net/wpu?retryWrites=true&w=majority";
 const client = new MongoClient(connectionString, { 
  useUnifiedTopology: true
 });
